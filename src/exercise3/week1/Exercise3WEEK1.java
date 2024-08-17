12de7924ef97379ef3a27178da3f5a1dd6dd4fe6
mauri <mauri@DESKTOP-JQLRDLV>
2024-08-16 20:57:25 -0500

    W

---
+++ src/exercise3/week1/Exercise3WEEK1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise3.week1;
+
+/**
+ *
+ * @author mauri
+ */
+public class Exercise3WEEK1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
